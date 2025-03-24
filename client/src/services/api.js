0a64c1751fbf56b0eea4382736a0e74f7f511548
Avi7877489 <avi7877489@gmail.com>
2025-03-24 12:01:13 +0530

    Frontend env update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_BASE_URL || "https://employee-management-4-ykfq.onrender.com/";
 
 const api = axios.create({
   baseURL: API_URL,
